6f8bfdcf3461b2c4f42f713852b7e0cfabbc9f67
张佳辉 <945276949@qq.com>
2020-05-19 10:55:09 +0800

    测试代码

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      //   this.$router.push('/login')
+      //   格式已经处理 双引号会自动变成单引号  分号会自动清除
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+import './assets/fonts/iconfont.css'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+// 导入字体图标
+// import './assets/iconfont/iconfont.js'
+// import './assets/iconfont/iconfont.css'
+
+// 我是注释
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
